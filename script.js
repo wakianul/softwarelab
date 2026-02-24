80e310f9e8ac07e93fba76f77efdc5d45d916d73
wakianul <xurjing.luna@gmail.com>
2026-02-24 00:22:45 -0500

    Second Set Up

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 function responsivePoster() {
   const text1 = document.querySelector("#mainText1");
   const text2 = document.querySelector("#mainText2");
+  const text3 = document.querySelector("#mainText3");
 
   // 防止找不到元素报错
   if (!text1 || !text2) return;
 
-  if (window.matchMedia("(max-width: 700px)").matches) {
-    text1.textContent = "Event";
+  
+  if (window.matchMedia("(max-width: 800px)").matches) {
+    text1.textContent = "This Friday at";
     text2.textContent = "4:30 PM";
-  } 
+  }
+
   else if (window.matchMedia("(max-height: 700px)").matches) {
     text1.textContent = "Tiri Kananuruk is a Thai performance artist and educator based in New York. Her work focuses on the relationships between sound, text, and time, investigating how technologies shift meaning and transform the ways we perceive and communicate. Mistakes are a key concept in her practice, both human and machine, as a space for making choices, improvisation, rupture, and creating unexpected moments that hold an audience.";
-    text2.textContent = "Friday";
+    text2.textContent = "Intro";
+    text3.textContent = "https://morakana.com";
   } 
+
   else {
-    text1.textContent = "Tiri Kananuruk Design Talk";
+    text1.textContent = "Tiri Kananuruk";
     text2.textContent = "Feb 27";
+    text3.textContent = "Design Talk"
   }
 }
 
